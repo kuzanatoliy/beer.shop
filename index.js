d2e9c278fe111567b91c705aefa36f58cbc555a5
Anatoli Kuzmiankou <Anatoli_Kuzmiankou@epam.com>
2018-01-10 18:16:44 +0300

    Remove configs

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const configs = require('./configs');
-
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 app.use(bodyParser());
 
 app.use('/', (req, res) => {
-  res.render('index', configs);
+  res.render('index', {});
+});
+
+app.get('/log-in', (req, res) => {
+  res.render('index', {});
+});
+
+app.get('/sing-up', (req, res) => {
+  res.render('index', {});
 });
 
 app.listen(8888);
